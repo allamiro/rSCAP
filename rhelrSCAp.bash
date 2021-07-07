4367b54577fda75c40a17745903659b48c2b0964
Tamir Suliman <allamiro@gmail.com>
2021-07-07 16:59:17 +0300

    Update rhelrSCAp.bash

--- rhelrSCAp.bash
+++ rhelrSCAp.bash
@@ -1,1 +1,19 @@
+#!/bin/bash
+
+
+
+
+
+
+# Results Variables
+
+export TYPE=`echo $1 | grep "-"|cut -d"-" -f1`
+export STG_VERSION=`echo $1 |grep V | cut -d "_" -f2`
+export TODAY_DT=`date +%Y-%m-%d`
+export func_scripts="functions/"
+export N_CHECKS="stig_checks/$TYPE/$STG_VERSION/$TYPE_$TODAY_DT"
+export N_RESULTS="results/$TYPE/$STG_VERSION/$TYPE_$TODAY_DT/" && mkdir -p $N_RESULTS
+
+
+
 
